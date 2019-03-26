169863ce6be7c796beb88d204777c5bd8098f98b
Kevin Kim <kevintaekim80@gmail.com>
2019-03-26 11:09:35 -0700

    import Grid component to main App and render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//app component imports
+import Board from './components/Board';
+
 class App extends Component {
   render() {
     return (
       <div className="app-container">
         <h2>React: Tic-Tac-Toe App</h2>
+        <Board />
       </div>
     );
   }
