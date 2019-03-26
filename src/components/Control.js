fb3fe94a6141b748c26076bcf61794b131c15695
Kevin Kim <kevintaekim80@gmail.com>
2019-03-26 15:26:56 -0700

    update styling to center all components to page

--- src/components/Control.js
+++ src/components/Control.js
@@ -17,11 +17,13 @@ const Control = (props) => {
 
   return (
     <div className="control-container">
-    	<p>Past record: Player: {playerPastScore} | Computer: {computerPastScore}</p>
-      <p>Current Player: {currentPlayer}</p>
-      <p>Winner: {winner ? `${currentPlayerKey} is the winner!`: 'No winner yet'}</p>
-      <button className="btn btn-primary" onClick={resetGame}>Reset Game</button>
-      <button className="btn btn-warning" onClick={resetPastScores}>Reset Past History</button>
+    	<p><strong>Past Record:</strong> Player: {playerPastScore} | Computer: {computerPastScore}</p>
+      <p><strong>Turn:</strong> {currentPlayerKey.toUpperCase()}</p>
+      <p><strong>Winner:</strong> {winner ? `${currentPlayerKey.toUpperCase()} is the winner!`: 'No winner yet'}</p>
+      <div className="control-buttons">
+	      <button className="btn btn-primary" onClick={resetGame}>Reset Game</button>
+	      <button className="btn btn-warning" onClick={resetPastScores}>Reset Past History</button>
+      </div>
     </div>
   );
 
